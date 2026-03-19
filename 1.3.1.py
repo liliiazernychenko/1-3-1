3d20cdcd985cee530211931458b988c65d2eeb07
liliiazernychenko <lilavaon@gmail.com>
2026-03-19 19:06:52 +0200

    1.3.1.py

--- 1.3.1.py
+++ 1.3.1.py
@@ -6,20 +6,28 @@ class Triangle:
         self.c = c
     def perimeter(self):
         return self.a + self.b + self.c
-    
+    def area(self):
+        if (self.a + self.b <= self.c or
+            self.a + self.c <= self.b or
+            self.b + self.c <= self.a):
+            return 0
+        p = self.perimeter() / 2
+        return math.sqrt(p * (p - self.a) * (p - self.b) * (p - self.c))
 class Rectangle:
     def __init__(self, a, b):
         self.a = a
         self.b = b
     def perimeter(self):
         return 2 * (self.a + self.b)
-
+    def area(self):
+        return self.a * self.b
 class Circle:
     def __init__(self, r):
         self.r = r
     def perimeter(self):
         return 2 * math.pi * self.r
-
+    def area(self):
+        return math.pi * self.r ** 2
 class Parallelogram:
     def __init__(self, a, b, h):
         self.a = a
@@ -27,7 +35,8 @@ class Parallelogram:
         self.h = h
     def perimeter(self):
         return 2 * (self.a + self.b)
-
+    def area(self):
+        return self.a * self.h
 class Trapeze:
     def __init__(self, a, b, c, d):
         self.a = a
@@ -36,11 +45,14 @@ class Trapeze:
         self.d = d
     def perimeter(self):
         return self.a + self.b + self.c + self.d
-
+    def area(self):
+        return 0   # площу не задавали — залишаємо 0
 def process_file(filename):
     file = open(filename)
     max_perimeter = 0
-    max_figure = ""
+    max_area = 0
+    max_p_fig = ""
+    max_a_fig = ""
     for line in file:
         parts = line.split()
         name = parts[0]
@@ -55,19 +67,23 @@ def process_file(filename):
         elif name == "Trapeze":
             fig = Trapeze(float(parts[1]), float(parts[2]), float(parts[3]), float(parts[4]))
         p = fig.perimeter()
+        a = fig.area()
         if p > max_perimeter:
             max_perimeter = p
-            max_figure = line.strip()
-
+            max_p_fig = line.strip()
+        if a > max_area:
+            max_area = a
+            max_a_fig = line.strip()
     file.close()
-
     print("Файл:", filename)
     print("Фігура з найбільшим периметром:")
-    print(max_figure)
-    print("Периметр =", max_perimeter)
+    print(max_p_fig)
+    print("Периметр =", round(max_perimeter, 2))
+    print("Фігура з найбільшою площею:")
+    print(max_a_fig)
+    print("Площа =", round(max_area, 2))
     print()
-
-
 process_file("input01.txt")
 process_file("input02.txt")
 process_file("input03.txt")
+
